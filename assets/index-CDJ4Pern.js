6b7bdbb98188d06034c9cb6839a175fff27293c2
jameshsu1125 <jameshsu1125@gmail.com>
2024-01-23 15:33:12 +0800

    Updates

---
+++ assets/index-CDJ4Pern.js
@@ -0,0 +1,1 @@
+import{b as s,j as e}from"./index-ZR3i4SzE.js";const i=s.memo(()=>e.jsx("section",{className:"Tie",style:{height:window.innerHeight},children:e.jsx("div",{})}));export{i as default};
