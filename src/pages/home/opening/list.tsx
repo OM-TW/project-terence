702d5ac2821eca13d8187d9f2b9b68858c3e5768
jameshsu1125 <jameshsu1125@gmail.com>
2025-02-11 15:50:37 +0800

    新增多個組件並移除不必要的檔案

--- src/pages/home/opening/list.tsx
+++ src/pages/home/opening/list.tsx
@@ -8,15 +8,18 @@ const List = memo(() => {
     <div className='List'>
       <div>
         <ol>
-          {['業務', '策略', '創意', '公關', '數位體驗'].map((name, i) => (
-            <li key={name} className={state.index === i ? 'active' : ''}>
-              <button
-                onClick={() => {
-                  setState((S) => ({ ...S, index: i }));
-                }}
-              >{`${name}紅領帶`}</button>
-            </li>
-          ))}
+          {['業務', '策略', '創意', '公關', '數位體驗'].map((name, i) => {
+            if (name === '公關') return null;
+            return (
+              <li key={name} className={state.index === i ? 'active' : ''}>
+                <button
+                  onClick={() => {
+                    setState((S) => ({ ...S, index: i }));
+                  }}
+                >{`${name}紅領帶`}</button>
+              </li>
+            );
+          })}
         </ol>
       </div>
     </div>
