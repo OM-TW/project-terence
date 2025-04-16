a9ccd610f334205df1899d2ffa635daba2e68e69
jameshsu1125 <jameshsu1125@gmail.com>
2025-04-16 15:09:21 +0800

    Refactor code structure for improved readability and maintainability

--- src/components/news/dialog.tsx
+++ src/components/news/dialog.tsx
@@ -1,7 +1,7 @@
 import { Context } from '@/settings/constant';
 import { ActionType, TransitionType } from '@/settings/type';
 import useTween from 'lesca-use-tween';
-import { memo, useContext, useEffect, useRef } from 'react';
+import { memo, useContext, useEffect, useMemo, useRef } from 'react';
 import Button from '../button';
 
 type T = {
@@ -14,6 +14,24 @@ const Dialog = memo(({ transition, onClose, html }: T) => {
   const [, setContext] = useContext(Context);
   const ref = useRef<HTMLDivElement>(null);
   const [style, setStyle] = useTween({ y: window.innerHeight });
+
+  const currentHTML = useMemo(() => {
+    if (html) {
+      const tempDiv = document.createElement('div');
+      tempDiv.innerHTML = html;
+      const blockquote = tempDiv.querySelector('blockquote');
+      if (blockquote) {
+        const textArray = blockquote.innerText.split(',');
+        const spanElements = textArray.map((text) => `<span>${text.trim()}</span>`).join('');
+        const div = document.createElement('div');
+        div.classList.add('grid', 'grid-cols-3', 'lg:grid-cols-6', 'gap-2', 'lg:px-40', 'px-10');
+        div.innerHTML = spanElements;
+        blockquote.replaceWith(div);
+      }
+      return tempDiv.innerHTML;
+    }
+    return html || '';
+  }, [html]);
 
   useEffect(() => {
     if (transition === TransitionType.FadeIn) setStyle({ y: 0 }, { duration: 500, delay: 200 });
@@ -32,11 +50,12 @@ const Dialog = memo(({ transition, onClose, html }: T) => {
 
   return (
     <div className='dialog' style={style}>
-      <div ref={ref} className='content' dangerouslySetInnerHTML={{ __html: html || '' }} />
+      <div ref={ref} className='content' dangerouslySetInnerHTML={{ __html: currentHTML || '' }} />
       <div className='close'>
         <Button onClick={onClose}>
           <Button.Close />
         </Button>
+        <div className='hidden px-10 lg:px-40' />
       </div>
     </div>
   );
